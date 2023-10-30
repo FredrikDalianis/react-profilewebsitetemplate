9d25fd82248913be09422ef755a80e6d2bdb257a
FredrikDalianis <78905580+FredrikDalianis@users.noreply.github.com>
2023-10-30 07:07:07 +0100

    skills

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -3,7 +3,19 @@ import Data from '../../data/index.json';
 export default function MySkills(){
     return(
         <section className="skills--section" id="mySkills">
-        Hello
+        <div className='portfolio--container'>
+            <p className='section--title'>My Skills</p>
+            <h2 className='skills--section--heading'>My Expertise</h2>
+        </div>
+        
+        <div className='skills--section--container'> 
+        {Data?.skills?.map((item,index) =>(<div key={index} className='skills--section--card'>
+            <div className='skills--section--img'>
+                <img src={item.src} alt="Skill item image " />
+            </div>
+        </div>
+        ))}
+        </div>
         </section>
     )
 }
